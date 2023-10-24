9da01d7994b84b4065c9e7aac76bdea84133a950
Sushant Patil <sp2112@cam.ac.uk>
2023-10-24 07:32:18 +0100

    Create remove_vertex method

--- graph.py
+++ graph.py
@@ -29,3 +29,10 @@ class Graph:
             return True
         return False
 
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
